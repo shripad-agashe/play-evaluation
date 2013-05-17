922100194782c918c9eceab5e3ee6c15a1610705
Shripad Agashe <shripad.agashe@gmail.com>
2013-05-17 11:13:39 +0530

    Shripad - Resolving errors

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Plugins extends Build {
 	lazy val plugins = Project("plugins", file("."))
-    .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"
+    .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"))
 }
