6ed9a92d9dbd1f89f34d015ba2db1665e98f5588
Shripad Agashe <shripad.agashe@gmail.com>
2013-05-17 11:10:37 +0530

    Shripad - removed comment

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -2,9 +2,5 @@ import sbt._
 
 object Plugins extends Build {
 	lazy val plugins = Project("plugins", file("."))
-    .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"))
-
-    #adding some comment
-
-
+    .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"
 }
