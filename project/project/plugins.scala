f73d64944d3846d88f6eee1d22d5af126ab54d40
Shripad Agashe <shripad.agashe@gmail.com>
2013-05-17 11:09:06 +0530

    Shripad - Adding plugins.scala in project/project folder

---
+++ project/project/plugins.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+object Plugins extends Build {
+	lazy val plugins = Project("plugins", file("."))
+    .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"))
+
+    #adding some comment
+
+
+}
