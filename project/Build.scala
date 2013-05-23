6d4d3f95b4f314ce1ebdd5f982d625e9e42872e8
jetinder20 <jetinder.singhrathore@gmail.com>
2013-05-23 14:40:13 +0530

    Jetinder | Basu => added sbt task 'dist-custom'

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,4 @@
+import java.io.File
 import sbt._
 import Keys._
 import play.Project._
@@ -11,33 +12,54 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     "org.scalatest" % "scalatest_2.10" % "2.0.M5b" % "test",
-    "org.seleniumhq.selenium" % "selenium-java" % "2.32.0" % "test" ,
+    "org.seleniumhq.selenium" % "selenium-java" % "2.32.0" % "test",
     "org.fusesource.scalate" % "scalate-core_2.10" % "1.6.1"
   )
 
 
+  val hello = TaskKey[Unit]("hello", "Prints 'Hello World'")
+
+  val helloTask = hello := {
+    println("Hello World")
+  }
+
+  val customDist = TaskKey[Unit]("dist-custom", "Packs additional files in the distribution zip")
+
+  val someTask = customDist <<= (dist, distDirectory) map {(originalZip, originalDir) =>
+    val viewsDir = new File("./app/views")
+
+    IO.unzip(originalZip, originalDir)
+
+    val unzippedDir = new File(originalDir, appName + "-" + appVersion)
+    IO.copyDirectory(viewsDir, unzippedDir)
+
+    def filesToZip(files : Array[File]) : List[(File, String)] = {
+      files.foldLeft(List[(File, String)]()) {
+        (accumulator, file) => {
+          val relativePath = file.getAbsolutePath.substring(file.getAbsolutePath.indexOf("play-evaluation-1.0-SNAPSHOT"))
+          if (file.isDirectory)
+            (file, relativePath) :: filesToZip(file.listFiles()) ::: accumulator
+          else
+            (file, relativePath) :: accumulator
+        }
+      }
+    }
+
+    IO.zip(filesToZip(Array(unzippedDir)), originalZip)
+    IO.delete(unzippedDir)
+  }
 
 
 
-
-
-  val main = play.Project(appName, appVersion, appDependencies)
-    .settings(jasmineSettings : _*)
+  val main = play.Project(appName, appVersion, appDependencies, settings = Seq(someTask) ++ Defaults.defaultSettings)
+    .settings(jasmineSettings: _*)
     .settings(
-
-
-
-
-      appJsDir <+= resourceManaged / "main/public/javascripts",
-
-      appJsLibDir <+= baseDirectory / "public/javascripts",
-
-
-      jasmineTestDir <+=  resourceManaged / "test/public",
-      jasmineConfFile <+= baseDirectory / "test/assets/test.dependencies.js",
-
-      // link jasmine to the standard 'sbt test' action. Now when running 'test' jasmine tests will be run, and if they pass
-      // then other Play tests will be executed.
-      (test in Test) <<= (test in Test) dependsOn (jasmine)
-      )
+    appJsDir <+= resourceManaged / "main/public/javascripts",
+    appJsLibDir <+= baseDirectory / "public/javascripts",
+    jasmineTestDir <+= resourceManaged / "test/public",
+    jasmineConfFile <+= baseDirectory / "test/assets/test.dependencies.js",
+    // link jasmine to the standard 'sbt test' action. Now when running 'test' jasmine tests will be run, and if they pass
+    // then other Play tests will be executed.
+    (test in Test) <<= (test in Test) dependsOn (jasmine)
+  )
 }
